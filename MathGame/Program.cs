cc12e4eb44c1656a872092eaad57cd939b58d146
Alfonso <Alfonso@DESKTOP-2GP4ASB>
2025-09-12 19:41:27 -0600

    Agregar archivos de proyecto.

---
+++ MathGame/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string greeting = "Wassup cuh, this is a calculator!";
+
+if (true)
+{
+    Console.WriteLine(greeting);
+
+    Console.WriteLine("Write a number");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Write another number");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("The sum of the numbers is: " + (number1 + number2));
+}
+
