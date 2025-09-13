e604635c28f8ed1e2797a17cefb9323c4bdb2a5c
Alfonso Zendejas <backdown.97@gmail.com>
2025-09-12 20:33:04 -0600

    Changed the result message.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -11,6 +11,6 @@ if (true)
     Console.WriteLine("Write another number");
     int number2 = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("The multiplication of the numbers is: " + (number1 * number2));
+    Console.WriteLine("The result of the multiplication of the numbers is: " + (number1 * number2));
 }
 
